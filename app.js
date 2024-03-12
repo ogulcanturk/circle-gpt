db8bdff6c5e03e56417f19d3a26153276a002bdf
Ogulcan <account.work@ogulcan.dev>
2024-03-12 19:27:54 +0300

    upstash and openai services added

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(3000, () => {
-  console.log("NomadGPT listening on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`NomadGPT listening on port ${process.env.PORT || 3000}`);
 });
