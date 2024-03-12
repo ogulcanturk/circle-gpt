e016bf684cc479b03b5c48bbf59e3fd907590211
Ogulcan <account.work@ogulcan.dev>
2024-03-12 18:10:37 +0300

    added env.example

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Express
-const express = require('express');
+const express = require("express");
+
+// ENV
+require('dotenv').config();
 
 // App
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send('Hello World!')
 });
 
 app.listen(3000, () => {
-  console.log('NomadGPT listening on port 3000');
+  console.log("NomadGPT listening on port 3000");
 });
