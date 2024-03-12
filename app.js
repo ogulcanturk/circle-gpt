61a4edd1fc97dd049bbd999a892df4063a632fc3
Ogulcan <account.work@ogulcan.dev>
2024-03-12 17:28:25 +0300

    express added

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+// Express
+const express = require('express');
 
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(3000, () => {
+  console.log('NomadGPT listening on port 3000');
+});
